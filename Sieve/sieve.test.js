9c60e2be56885d43fca6583ebb3e32d0b254a052
“Joshthefrontendguy” <josh@sioncentral.com>
2025-01-30 11:35:16 -0500

    feat: added a few more test cases

--- Sieve/sieve.test.js
+++ Sieve/sieve.test.js
@@ -27,6 +27,21 @@ describe("Sieve", () => {
       expect(Sieve.NthPrime(1000000)).toBe(15485867);
       expect(Sieve.NthPrime(10000000)).toBe(179424691);
       // expect(Sieve.NthPrime(100000000)).toBe(2038074751); // Uncomment if you want to wait an hour :)
+
+      // Small indices
+      expect(Sieve.NthPrime(1)).toBe(3);   // 1st prime
+      expect(Sieve.NthPrime(2)).toBe(5);   // 2nd prime
+      expect(Sieve.NthPrime(3)).toBe(7);   // 3rd prime
+      expect(Sieve.NthPrime(4)).toBe(11);  // 4th prime
+      
+      // Some random checks
+      expect(Sieve.NthPrime(10)).toBe(31); // 10th prime
+      expect(Sieve.NthPrime(24)).toBe(97); // 25th prime
+      
+      // Negative checks.
+      expect(() => Sieve.NthPrime(-1)).toThrow("Index cannot be negative.");
+      expect(() => Sieve.NthPrime(-5)).toThrow("Index cannot be negative.");
+
     } catch (error) {
       testFailed = true;
       throw error; // Rethrow so Jest sees the test as failed
